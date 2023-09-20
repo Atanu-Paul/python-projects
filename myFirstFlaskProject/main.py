a51b6657f60f1df29df10ccc801b9b66b7085c12
PC-127 <atanu@matrixnmedia.com>
2023-09-20 20:35:07 +0530

    removed the old env folder and setup added new one with py3.8 and ubuntu settings, added first api routes

--- myFirstFlaskProject/main.py
+++ myFirstFlaskProject/main.py
@@ -1,8 +1,24 @@
 from flask import Flask
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
 
 @app.route('/')
 def hello_world():
     return '<p>Hello, World!!!!-1</p>'
+
+
+@app.route('/about-us')
+def about_us():
+    return '<p>This is a demo site 1111'
+
+class HelloWorldApi(Resource):
+    def get(self):
+        return {'status':'200', 'error':'false', 'message' : 'hello world!'}
+    
+api.add_resource(HelloWorldApi,'/api/hello')
+
+if __name__=='__main__':
+    app.run(debug=True)
